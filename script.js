faa1996ec60c47398ff58542b31a5339664335fa
Carolin Jenner <a.carolinjenner@gmail.com>
2022-11-12 13:41:29 +0100

    feat: add initial styling

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = ;
+const state = {
+    toDos: [{
+        description: "XXX",
+        done: false,
+    },
+    {
+        description: "XXX2",
+        done: false,
+    }]
+}
+
+// add new "your to dos"
+/*document.appendChild()*/
+
+// state anlegen
+// input mit Button "add" verbinden
+// wenn button geklickt: imput über "appendChild" in "new-to-dos"
+// reset button (delete all new-to-dos)
+// Filter (wenn "open": zeige open,...)
+// filter: nur 1 Objekt gleichzeitig klickbar
+// filter: default - "all" is activated
+// to-dos als done markieren
